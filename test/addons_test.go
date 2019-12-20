efd088014c1a65e32d6a6ec849064bf0b717527a
Jared Rodriguez <jared@blacknode.net>
2019-12-20 14:08:52 -0600

    working commit: transfer

--- test/addons_test.go
+++ test/addons_test.go
@@ -1,24 +1,15 @@
 package test
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"fmt"
-	"io/ioutil"
-	"math/big"
-	"os"
-	"os/exec"
-	"path"
-	"testing"
-	"time"
-
 	"github.com/blang/semver"
-	"gopkg.in/yaml.v2"
-
 	"github.com/mesosphere/kubeaddons/hack/temp"
+	"github.com/mesosphere/kubeaddons/pkg/repositories/local"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"testing"
+
 	"github.com/mesosphere/kubeaddons/pkg/api/v1beta1"
 	"github.com/mesosphere/kubeaddons/pkg/test"
 	"github.com/mesosphere/kubeaddons/pkg/test/cluster/kind"
@@ -33,7 +24,7 @@ type TestGroup struct {
 }
 
 const (
-	defaultKonvoyPath = "konvoy"
+	defaultKonvoyPath = "/Users/jared/git/konvoy/out-darwin/konvoy"
 )
 
 var addonTestingGroups = make(map[string]TestGroup)
@@ -134,9 +125,6 @@ func testgroup(t *testing.T, groupname string) error {
 		return err
 	}
 
-	if err := deployCertManagerCA(cluster); err != nil {
-		return err
-	}
 	addons, err := addons(addonTestingGroups[groupname].Addons...)
 	if err != nil {
 		return err
@@ -156,8 +144,11 @@ func testgroup(t *testing.T, groupname string) error {
 
 func addons(names ...string) ([]v1beta1.AddonInterface, error) {
 	var testAddons []v1beta1.AddonInterface
-
-	addons, err := temp.Addons("../addons/")
+	repo, err  := local.NewRepository("base", "../addons")
+	if err != nil {
+		return testAddons, err
+	}
+	addons, err := repo.ListAddons()
 	if err != nil {
 		return testAddons, err
 	}
@@ -181,7 +172,12 @@ func addons(names ...string) ([]v1beta1.AddonInterface, error) {
 func findUnhandled() ([]v1beta1.AddonInterface, error) {
 	var unhandled []v1beta1.AddonInterface
 
-	addons, err := temp.Addons("../addons/")
+	repo, err := local.NewRepository("base", "../addons")
+	if err != nil {
+		return unhandled, err
+	}
+
+	addons, err := repo.ListAddons()
 	if err != nil {
 		return unhandled, err
 	}
@@ -202,72 +198,6 @@ func findUnhandled() ([]v1beta1.AddonInterface, error) {
 	}
 
 	return unhandled, nil
-}
-
-func deployCertManagerCA(cluster test.Cluster) error {
-	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "namespace", "cert-manager"); err != nil {
-		return err
-	}
-
-	// create secret
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(1653),
-		Subject: pkix.Name{
-			Organization: []string{"d2iq"},
-			Country:      []string{"US"},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-
-	priv, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return err
-	}
-	pub := &priv.PublicKey
-	ca_b, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
-
-	certPath := path.Join(wd, "ca.crt")
-	keyPath := path.Join(wd, "ca.key")
-
-	certOut, err := os.Create(certPath)
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: ca_b})
-	certOut.Close()
-
-	// Private key
-	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-	keyOut.Close()
-
-	// create kubernetes-root-ca secret
-	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "secret", "tls", "kubernetes-root-ca", "--namespace=cert-manager", fmt.Sprintf("--cert=%s", certPath), fmt.Sprintf("--key=%s", keyPath)); err != nil {
-		return err
-	}
-
-	// create konvoyconfig-kubeaddons configmap
-	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "configmap", "konvoyconfig-kubeaddons", "--namespace=kubeaddons", "--from-literal=clusterHostname=kommander.example.com"); err != nil {
-		return err
-	}
-
-	defer os.Remove(certPath)
-	defer os.Remove(keyPath)
-	return nil
-}
-
-func kubectl(args ...string) error {
-	cmd := exec.Command("kubectl", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
 }
 
 // -----------------------------------------------------------------------------
